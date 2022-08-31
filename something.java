c901fa15f8c61f165760fd4f10b9aea9b8d960d0
Chirag Dixit <97054098+Chirag-Dixit@users.noreply.github.com>
2022-09-01 02:30:27 +0530

    Update something.java

--- something.java
+++ something.java
@@ -3,6 +3,6 @@ public class ABC
 public static void main(String[] args)
 {
 int x = 3, y = 7;
-System.out.println("Sum of x and y is "+(x+y));
+System.out.println("Sum of x and y is "+(x+y));//i am making a change for me to understand git
 }
 }
